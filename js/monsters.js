0146627d15c389c577476c2bb93c1ca4481ef2ef
Dave Sarbora <dsarbora@gmail.com>
2019-02-06 22:22:35 -0800

    fix bugs, change display feature to include more loops

--- js/monsters.js
+++ js/monsters.js
@@ -29,9 +29,13 @@ Monster.prototype.checkDead = function(){
 Monster.prototype.dropLoot = function(){
   if(game.characterLocation().location == 4){
     if(character.quest == true){
-      game.characterLocation().spawnItem(2)
+      game.characterLocation().spawnItem(2);
       game.characters[0].displayGetButton();
 
+    }
+    else{
+      game.characterLocation().spawnItem(3);
+      game.characters[0].displayGetButton()
     }
   }
 };
@@ -96,7 +100,7 @@ NPC.prototype.checkDead = function(){
 
 NPC.prototype.displayNPC = function(){
   if(this && !this.checkDead()){
-      $("#fightButton").show();
+      //$("#fightButton").show();
       $("#fightLog").append(this.name);
       $("#monsters").append("A friendly woman named " + this.name.toLowerCase() + " introduces herself."+ "<br><br>")
   }
